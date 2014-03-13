d7c11d04958c4124b3da9acf5e920a26f1ced819
f0o <f0o@devilcode.org>
2014-03-13 22:53:38 +0100

    back to the roots yeah

--- includes/alerts/bgp.inc.php
+++ includes/alerts/bgp.inc.php
@@ -2,7 +2,7 @@
  * Subject-Add: BGP Session {$alert['bgp']['state']}: {$alert['bgp']['bgpPeerIdentifier']}
  * Subject-Add: (AS {$alert['bgp']['bgpPeerRemoteAs']} - {$alert['bgp']['astext']})
  *
- * Format: BGP Session {$alert['bgp']['state']} since {formatUptime($alert['bgp']['bgpPeerFsmEstablishedTime'])}\n
+ * Format: BGP Session {$alert['bgp']['state']} since {$alert['bgp']['bgpPeerFsmEstablishedTime']}\n
  * Format: Hostname  : {$alert['device']['hostname']}\n
  * Format: Peer IP   : {$alert['bgp']['bgpPeerIdentifier']}\n
  * Format: Remote AS : {$alert['bgp']['bgpPeerRemoteAs']} ({$alert['bgp']['astext']})
