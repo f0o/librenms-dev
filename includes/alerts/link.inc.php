e7326ce21f55dfff37de9ef6c848d1545c4e915f
f0o <f0o@devilcode.org>
2014-03-14 11:54:10 +0100

    Moar to commeth!

--- includes/alerts/link.inc.php
+++ includes/alerts/link.inc.php
@@ -1,15 +1,27 @@
 /*
- * Format: $alert["device"]["hostname"]/$alert["port"]["ifName"] went
- * Format: from $alert["link"]["old"] to $alert["link"]["state"] at $alert["timestamp"]
+ * Short-Desc: Alert handler for Link-Typed events
+ * Require: device, port
+ *
+ * Subject: Port state change of {$alert['port']['ifDescr']} on {$alert['device']['hostname']}
+ * Format: {$alert['device']['hostname']}/{$alert['port']['ifDesc']} went
+ * Format: from {$alert['link']['old']} to {$alert['link']['state']}
+ *
+ * Subject-threshold: Port saturation threshold reached on {$alert['device']['hostname']}/{$alert['port']['ifDescr']}
+ * Format-threshold: Port saturation threshold alarm: {$alert['device']['hostname']} on {$alert['port']['ifDescr']}\n
+ * Format-threshold: Rates   : {$alert['threshold']['ifInBits_rate']}/{$alert['threshold']['ifOutBits_rate']\n
+ * Format-threshold: ifSpeed : {$alert['port']['ifSpeed']}
  */
-if( $state == "down" ) {
-	$ret["state"] = "down";
-	$ret["old"] = "up";
+
+if( $state == "threshold" ) {
+	$ret['ifInBits_rate'] = $extra['ifInBits_rate'];
+	$ret['ifOutBits_rate'] = $extra['ifOutBits_rate'];
+} elseif( $state == "down" ) {
+	$ret['state'] = "down";
+	$ret['old'] = "up";
 } elseif( $state == "up" ) {
-	$ret["state"] = "up";
-	$ret["old"] = "down";
+	$ret['state'] = "up";
+	$ret['old'] = "down";
 } else {
-	$ret["state"] = "unknown/$state";
-	$ret["old"] = "-";
+	return false;
 }
 return $ret;
