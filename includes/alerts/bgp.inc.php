1320c791db82772660782d551db971c3371536dd
f0o <f0o@devilcode.org>
2014-03-14 19:38:50 +0100

    swop swop

--- includes/alerts/bgp.inc.php
+++ includes/alerts/bgp.inc.php
@@ -1,15 +1,16 @@
 /*
- * Subject: BGP Session {$alert['bgp']['state']}: {$alert['bgp']['bgpPeerIdentifier']}
- * Subject: (AS {$alert['bgp']['bgpPeerRemoteAs']} - {$alert['bgp']['astext']})
+ * Subject: BGP Session {$alert['bgp']['state']}: {$alert['bgp']['peer']['bgpPeerIdentifier']}
+ * Subject: (AS {$alert['bgp']['peer']['bgpPeerRemoteAs']} - {$alert['bgp']['peer']['astext']})
  *
- * Format: BGP Session {$alert['bgp']['state']} since {$alert['bgp']['bgpPeerFsmEstablishedTime']}\n
+ * Format: BGP Session {$alert['bgp']['state']} since {$alert['bgp']['FsmEstablishedTime']}\n
  * Format: Hostname  : {$alert['device']['hostname']}\n
- * Format: Peer IP   : {$alert['bgp']['bgpPeerIdentifier']}\n
- * Format: Remote AS : {$alert['bgp']['bgpPeerRemoteAs']} ({$alert['bgp']['astext']})
+ * Format: Peer IP   : {$alert['bgp']['peer']['bgpPeerIdentifier']}\n
+ * Format: Remote AS : {$alert['bgp']['peer']['bgpPeerRemoteAs']} ({$alert['bgp']['peer']['astext']})
  */
 if( $state == "flap" || $state == "up" || $state == "down") {
 	$ret["state"] = $state;
-	$ret = array_merge( $ret, $extra );
+	$ret["peer"] = $extra["peer"];
+	$ret["FsmEstablishedTime"] = $extra["FsmEstablishedTime"];
 } else {
 	return false;
 }
