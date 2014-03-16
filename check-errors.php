b3bc909a2a5bc43375eaf45a4902b6f8e9a04e14
f0o <f0o@devilcode.org>
2014-03-16 20:44:19 +0100

    better quality much code

--- check-errors.php
+++ check-errors.php
@@ -1,20 +1,20 @@
 #!/usr/bin/env php
 <?php
-
-/**
- * Observium
- *
- *   This file is part of Observium.
- *
- * @package    observium
- * @subpackage alerts
- * @author     Adam Armstrong <adama@memetic.org>
- * @copyright  (C) 2006 - 2012 Adam Armstrong
- *
- */
+/* Copyright (C) 2014  <f0o@devilcode.org>
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>. */
 
 chdir(dirname($argv[0]));
-
 include("includes/defaults.inc.php");
 include("config.php");
 include("includes/definitions.inc.php");
@@ -22,33 +22,25 @@ include("includes/functions.php");
 include("html/includes/functions.inc.php");
 include("includes/alert.inc.php");
 
-// Check all of our interface RRD files for errors
+$params = NULL;
+if( is_number($argv[1]) ) {
+	$where = "AND `port_id` = ?";
+	$params = array($argv[1]);
+}
 
-if ($argv[1]) { $where = "AND `port_id` = ?"; $params = array($argv[1]); }
-
-$i = 0;
 $errored = array();
-
-foreach (dbFetchRows("SELECT * FROM `ports` AS I, `devices` AS D WHERE I.device_id = D.device_id $where", $params) as $interface)
-{
-  $errors = $interface['ifInErrors_delta'] + $interface['ifOutErrors_delta'];
-  if ($errors > 1)
-  {
-    $errored[] = array($interface['port_id'], $interface);
-  }
-  $i++;
+foreach( dbFetchRows("SELECT * FROM `ports` AS I, `devices` AS D WHERE I.device_id = D.device_id $where", $params) as $interface ) {
+	if( ($interface['ifInErrors_delta']+$interface['ifOutErrors_delta']) > 0 ) {
+		$errored[] = $interface;
+	}
 }
 
-echo("Checked $i interfaces\n");
-
-if (is_array($errored))
-{ // If there are errored ports
-  $i = 0;
-  $msg = "Interfaces with errors : \n\n";
-  echo $interface['hostname'].'/'.$interface['ifDescr']."\n";
-  $alert = new Alert( array( 'obj' => 'p'.$errored[0], 'type' => 'link', 'state' => 'error', 'extra' => $errored[1], 'issue' => true ) );
+echo "Checked $i interfaces\n";
+if( sizeof($errored) > 0 ) {
+	foreach( $errored as $interface ) {
+		echo $interface['hostname'].'/'.$interface['ifDescr']."\n";
+		$alert = new Alert( array( 'obj' => 'p'.$interface['port_id'], 'type' => 'link', 'state' => 'error', 'extra' => $interface, 'issue' => true ) );
+	}
 }
-
-echo("$errored interfaces with errors over the past 5 minutes.\n");
-
+echo sizeof($errored)." interfaces with errors over the past 5 minutes.\n";
 ?>
